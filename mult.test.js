dacd6f3b65ea1f4dcef3146955e7ffaf7e231460
Jovêncio Neto <151071159+JovencioNeto@users.noreply.github.com>
2025-02-06 14:49:21 -0300

    Add files via upload

--- mult.test.js
+++ mult.test.js
@@ -1,5 +1,5 @@
 const mult = require('./mult')
 
-test('A função multiplicação deve retornar -4 ao multplicar -2 e 2', ()=>{
+test('A função mult deve retornar -4 ao multplicar -2 e 2', ()=>{
     expect(mult(-2,2)).toBe(-4)
 })
--- soma.test.js
+++ soma.test.js
@@ -1,9 +1,9 @@
 const soma = require('./soma')
 
-test('A função soma deve retornar 5 ao somar 2 e 3', ()=>{
+test('A função soma deve retornar 5000000000 ao somar 2000000000 e 3000000000', ()=>{
     expect(soma(2000000000,3000000000)).toBe(5000000000)
 })
 
-test('A função soma deve retornar 2 ao somar 1 e 1', () =>{
+test('A função soma deve retornar 2000000000 ao somar 1000000000 e 1000000000', () =>{
     expect(soma(1000000000,1000000000)).toBe(2000000000)
 })
